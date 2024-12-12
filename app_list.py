77c745ab448eeaea2a11fe07e34f5a1ee7982809
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-11 19:15:46 -0800

    Update app_list.py

--- app_list.py
+++ app_list.py
@@ -1,25 +1,26 @@
 import streamlit as st
 
 st.title("Apps")
-st.write("These proof of concept apps demostrate the application of machine learning, from basic model to advanced AI model to assist with various tasks.")
+st.write("""The advancement of technology was intended to reduce officers' workload; however, evidence shows that officers remain overloaded with administrative tasks 
+            despite police departments' investments in modernization of various systems.
+            Machine learning and AI offer a promissing solution. These proof of concept apps demostrate the application of machine learning, from basic model to advanced AI model to assist with various tasks.
+            The main purpose of these concepts is to promote awareness and to help officers manage the significant increase in adminstrative tasks in the past decade."""
 
 st.markdown("<p><h3><span style='color: blue;'>Offence Classifier</h3></span></p>", unsafe_allow_html=True)
-st.write("""This machine learning model uses Logistic Regression with TF-IDF vectorizer to classify offenses. 
-            It was trained on fewer than 100 examples, and a larger dataset will improve accuracy. This model works locally and does not require connection to the internet. """)
+st.write("This is a basic machine learning model using Logistic Regression to classify offences. The mode was train on less than 100 examples.")
 st.markdown("[<h3>Try it</h3>](https://my-qc-app-7ij5uuv4vgp3nhyzrhttdm.streamlit.app/)", unsafe_allow_html=True)
 st.write("")
 
 st.markdown("<p><h3><span style='color: blue;'>Similarity Search</h3></span></p>", unsafe_allow_html=True)
-st.write("""This exercise demonstrates the application of Cosine Similarity using a pre-trained LLM model, specifically paraphrase-MiniLM-L6-v2, to identify similar texts. 
-            LLM models like this are designed to capture meanings and the context of the texts to a certain extend, making it useful to find similar MOs in a database and detect patterns in criminal activities. 
-            This model works locally and does not require connection to the internet. """)
+st.write("""This exercise demostrates the application of using Cosine Similarity from a pre-trained LLM model, specifically paraphrase-MiniLM-L6-v2 to identify similar texts. 
+            The concept can be used to find similar MOs in the database and can be useful in detecting patterns in criminal activities.""")
 st.markdown("[<h3>Try it</h3>](https://cs-transformer.streamlit.app/)", unsafe_allow_html=True)
 st.write("")
 
 st.markdown("<p><h3><span style='color: blue;'>eIM</h3></span></p>", unsafe_allow_html=True)
-st.write("""This proof of concept leverages an advanced AI model to generate an eIM compliant RTCC or any report with all entities and text pages in an XML file. 
-            The generated file can be imported for further processing or sent directly to the Transcription Queue for processing.  This model uses the free version of Gemini and all data is sent to the server for processing; 
-            therefore confidential information is not recommended. """)
+st.write("""This proof of concept uses the most advanced AI model to generate an eIM compliant RTCC or any report with
+            all entities and text pages in an XML file. The file can be imported for further processing or sent directly to
+            the Transcription Queue for further processing.""")
 st.markdown("[<h3>Try it</h3>](https://eim-api.streamlit.app/)", unsafe_allow_html=True)
 st.write("")
 
