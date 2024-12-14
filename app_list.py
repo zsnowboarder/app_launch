628f2bb1fbf845df95c21638d614d15b61f0e076
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-13 18:00:08 -0800

    Update app_list.py

--- app_list.py
+++ app_list.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Apps")
+st.title("Apps with endless possibilities")
 st.write("""The advancement of technology was intended to reduce officers' workload; however, evidence shows that officers remain overloaded with administrative tasks 
             despite police departments' investments in modernization of various systems.
             Machine learning and AI offer a promissing solution. These proof of concept apps demostrate the application of machine learning, from basic model to advanced AI model to assist with various tasks.
@@ -17,17 +17,20 @@ st.write("""This exercise demostrates the application of using Cosine Similarity
 st.markdown("[<h3>Try it</h3>](https://cs-transformer.streamlit.app/)", unsafe_allow_html=True)
 st.write("")
 
+st.markdown("<p><h3><span style='color: blue;'>Name Entity Recogition</h3></span></p>", unsafe_allow_html=True)
+st.write("""This demo shows the posiblity to using a pre-trained NER AI model to identify entities such as name, location, and mamy others within a text page. With this
+            in mind, the possibilities are up to what you want to achive! This model works locally and does not require connection to the internet. """)
+st.markdown("[<h3>Try it</h3>](https://ner-police.streamlit.app/)", unsafe_allow_html=True)
+st.write("")
+
 st.markdown("<p><h3><span style='color: blue;'>eIM</h3></span></p>", unsafe_allow_html=True)
 st.write("""This proof of concept uses the most advanced AI model to generate an eIM compliant RTCC or any report with
             all entities and text pages in an XML file. The file can be imported for further processing or sent directly to
             the Transcription Queue for further processing. This model uses the free version of Gemini and all data is sent to the server for processing; therefore confidential information is not recommended.""")
 st.markdown("[<h3>Try it</h3>](https://eim-api.streamlit.app/)", unsafe_allow_html=True)
 st.write("")
+st.markdown("**Although these demos show that these models work independently, it is possible to combine them for achieving certain goal. For example, NER can be used to remove confidential information prior to sending the text to the AI model for further processing.")
 
-st.markdown("<p><h3><span style='color: blue;'>Name Entity Recogition</h3></span></p>", unsafe_allow_html=True)
-st.write("""This demo shows the posiblity to using a pre-trained NER AI model to identify entities such as name, location, and mamy others within a text page. With this
-            in mind, the possibilities are up to what you want to achive! This model works locally and does not require connection to the internet. """)
-st.markdown("[<h3>Try it</h3>](https://ner-police.streamlit.app/)", unsafe_allow_html=True)
-st.write("")
+
 
          
