8120c686ec933b4d416706b662b3676e9ab9fd6d
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2025-01-18 19:26:24 -0800

    Update app_list.py

--- app_list.py
+++ app_list.py
@@ -20,7 +20,7 @@ greeting = get_greeting()
 #username = os.getenv('USERNAME') or os.getenv('USER')
 st.title(greeting)
 st.title("🤖 Apps with endless possibilities")
-st.title("Note: Sometimes apps go to sleep due to inactivity. If you are unable to wake it up or encounter memory issues due to limited usage in the free version, please contact Jimmy to reboot the app.")
+st.title("⚠️ These apps may occasionally go to sleep due to inactivity. If you are unable to wake it up or encounter memory issues caused by limitations in the free version, please contact Jimmy to reboot the app.")
 st.write("""one of the main purposes of advancement of technology should be to increase productivity and reduce officers' workload; 
             however, evidence has shown that officers remain overloaded with administrative tasks 
             despite police departments' investments in modernization of various systems. Police departments are often lagged behind in rapidly developing applications and tools
